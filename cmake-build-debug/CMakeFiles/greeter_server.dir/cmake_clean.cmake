8f35d70619463e56a662e13395d09f28228d949b
laiyijie <449132062@qq.com>
2017-08-02 10:41:43 +0800

    add build/proto-src to fix the ide problem

---
+++ cmake-build-debug/CMakeFiles/greeter_server.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "proto-src/helloworld.pb.cc"
+  "proto-src/helloworld.pb.h"
+  "proto-src/helloworld.grpc.pb.cc"
+  "proto-src/helloworld.grpc.pb.h"
+  "CMakeFiles/greeter_server.dir/greeter_server.cc.o"
+  "CMakeFiles/greeter_server.dir/proto-src/helloworld.pb.cc.o"
+  "CMakeFiles/greeter_server.dir/proto-src/helloworld.grpc.pb.cc.o"
+  "greeter_server.pdb"
+  "greeter_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greeter_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
